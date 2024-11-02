2d0f78b593a2517453ec59486826265c21ebad22
Studenti <eroskrasniqi02@gmail.com>
2024-11-02 11:44:35 +0100

    eros

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
-name="erosi"
-print(name)
-print(name)
+
+person = ('Eros',16,"Football")
+name,age,profesional= person
+
+print(name, "'s", "profesional is", profesional, "and she is", age,"years old")
