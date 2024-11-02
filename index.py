aaa959d24bf8eb5bf753baa61e17a23b53d994a2
Studenti <eroskrasniqi02@gmail.com>
2024-11-02 12:11:34 +0100

    eros

--- index.py
+++ index.py
@@ -2,4 +2,32 @@
 person = ('Eros',16,"Football")
 name,age,profesional= person
 
-print(name, "'s", "profesional is", profesional, "and she is", age,"years old")
+print(name, "'s", "profesional is", profesional, "and he is", age,"years old")
+
+my_dictionary = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+
+}
+
+contact_info = {
+    "Eros": "323-231311",
+    "Drilon": "293-12904"
+
+
+}
+
+Driloni_phone = contact_info["Drilon"]
+
+print(Driloni_phone)
+print(contact_info)
+
+contact_info["Drilon"] ="293-123234"
+
+print(contact_info)
+
+contact_info ["Eros"]= "323-12312"
+
+print(contact_info)
+
