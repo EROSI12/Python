5fa7dbdd808693c2f8e9bd2746f5402fd78b1cea
Student <eroskrasniqi02@gmail.com>
2024-12-07 03:48:31 -0800

    g

---
+++ Lesson 7/error_handling.py
@@ -0,0 +1,17 @@
+try:
+
+    result=10/10
+except:
+    print("this is not deviton to zero, click this name again")
+
+
+fruit = {
+   "apple":5,
+    "organe":5,
+    "banana":7,
+}
+
+try:
+    print(fruit(("cherry"));
+
+
---
+++ Lesson 7/index.py
@@ -0,0 +1,24 @@
+age = 25
+
+age_as_str= str(age)
+
+print(age_as_str,"of type",type(age_as_str))
+
+print(bool(""))#Output False
+print(bool("50"))#Output True
+
+print(bool([""]))#Output False
+print(bool([None]))#Output False
+
+x =85
+y=9.5
+resulat= x+y
+print(resulat,"of type",type(resulat))
+
+age =27
+message ="i am"+str(age)+ "year old"
+
+a=5
+b= "3"
+resulat = a+int(b)
+print(resulat)
